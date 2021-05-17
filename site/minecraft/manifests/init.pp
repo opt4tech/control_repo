dd742afeaa460afae95b3dcde86b183dabca14b9
opt4tech <79595336+opt4tech@users.noreply.github.com>
2021-05-17 08:58:41 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -16,11 +16,11 @@ class minecraft {
     ensure => file,
     content => 'eula=true',
   }
-  file {'/etc/systemd/system/minecraft.service':
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    conten +> epp{'minecraft/minecraft.service',{
+    content => epp(‘minecraft/minecraft.service.epp’, {
       install_dir => $install_dir,
-    }},
+    }),
   }
   service {'minecraft':
     ensure => running,
